ef23fafe6ce062a88600d44d6c817b87a45790ed
Maksim Sinyakov <masinyakov@avito.ru>
2019-10-20 11:22:08 +0300

    подключение шрифта с google.fonts

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,12 @@
-import { graphql, StaticQuery } from "gatsby";
-import React from "react";
+import { graphql, StaticQuery } from 'gatsby';
+import React from 'react';
+import Helmet from 'react-helmet';
 
-import "src/components/Layout/normalize.css";
-import "src/components/Layout/styles.css";
+import 'src/components/Layout/normalize.css';
+import 'src/components/Layout/styles.css';
 
-import Footer from "src/components/Layout/Footer";
-import Header from "src/components/Layout/Header";
+import Footer from 'src/components/Layout/Footer';
+import Header from 'src/components/Layout/Header';
 
 interface IProps {
   children: React.ReactNode | React.ReactNode[];
@@ -24,6 +25,9 @@ const Layout: React.SFC<IProps> = ({ children }) => (
     `}
     render={() => (
       <>
+        <Helmet>
+          <link href="https://fonts.googleapis.com/css?family=PT+Sans:400,700&amp;display=swap&amp;subset=cyrillic" rel="stylesheet" />
+        </Helmet>
         <Header />
         <main className="main-content">
           <div className="wrapper">{children}</div>
